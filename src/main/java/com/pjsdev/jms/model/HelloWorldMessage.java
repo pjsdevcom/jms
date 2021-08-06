5abbe8ce5c78adf2aba7dddf0825888fe3f4a5c8
pjsdevcom <mail@pjsdev.com>
2021-08-06 10:13:19 +0100

    Add HelloWorldMessage message object

---
+++ src/main/java/com/pjsdev/jms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package com.pjsdev.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 4128959642362136168L;
+
+    private UUID id;
+    private String message;
+}
