68c12660d29e063e472af2024b6a159d69e70b14
pjsdevcom <mail@pjsdev.com>
2021-08-10 21:18:24 +0100

    Add "sand and receive" of JMS message

--- src/main/java/com/pjsdev/jms/listener/HelloMessageListener.java
+++ src/main/java/com/pjsdev/jms/listener/HelloMessageListener.java
@@ -2,16 +2,23 @@ package com.pjsdev.jms.listener;
 
 import com.pjsdev.jms.config.JmsConfig;
 import com.pjsdev.jms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
@@ -20,5 +27,18 @@ public class HelloMessageListener {
 
         System.out.println("I got a message!");
         System.out.println(helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("World!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 }
--- src/main/java/com/pjsdev/jms/sender/HelloSender.java
+++ src/main/java/com/pjsdev/jms/sender/HelloSender.java
@@ -1,12 +1,18 @@
 package com.pjsdev.jms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pjsdev.jms.config.JmsConfig;
 import com.pjsdev.jms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @Component
@@ -14,6 +20,7 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
@@ -28,5 +35,32 @@ public class HelloSender {
         jmsTemplate.convertAndSend(JmsConfig.QUEUE, message);
 
         System.out.println("Message sent!");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        Message receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.SEND_RCV_QUEUE, session -> {
+            Message helloMessage;
+            try {
+                helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                helloMessage.setStringProperty("_type", "com.pjsdev.jms.model.HelloWorldMessage");
+
+                System.out.println("Sending Hello...");
+
+                return helloMessage;
+            } catch (JsonProcessingException e) {
+                throw new JMSException("boom");
+            }
+        });
+
+        if (receivedMsg != null) {
+            System.out.println(receivedMsg.getBody(String.class));
+        }
     }
 }
